6523bae4c848dd63c34eb917d1ebf60522e1328c
MaxBouricius <55273830+MaxBouricius@users.noreply.github.com>
2024-04-10 23:48:01 +0200

    Doorgewerkt aan het leensysteem

--- dashboard/functions.php
+++ dashboard/functions.php
@@ -3,7 +3,7 @@
 function dbconnect($database)
 {
 
-    $db_server = "mediacollege-hosts-st1.cust.webslice.eu";
+    $db_server = "127.0.0.1";
     $db_username = "c5831thijn";
     $db_password = "Th1jn-MA";
 
@@ -15,13 +15,14 @@ function dbconnect($database)
     mysqli_select_db($connection, $database);
     return $connection; 
 
+    
 }
 
 //in de check_login functie check ik de session id en maakt die vervolgens een nieuwe aan. ik geef via cookies een user_id, session id en het ip mee
 function check_login($user_id, $session, $ipv4)
 { 
     //eerst maakt hij een database connectie zodat ik in de database de gegevens kan checken
-    $dbconnection = dbconnect("stageblog");
+    $dbconnection = dbconnect("c5831Leensysteem");
     //ik select alles via het userid 
     $check_login = mysqli_query($dbconnection, "SELECT * FROM users WHERE id = '".$user_id."' AND session = '".$session."' AND ipv4 = '".$ipv4."' LIMIT 1"); 
     //deze if statement checkt of ik een row terug krijg
@@ -38,6 +39,21 @@ function check_login($user_id, $session, $ipv4)
         exit; 
     }
 
+}
+
+//met deze functie maak ik een simpele string aan die ik mee kan geven als session id dit doe ik met random
+function getRandomString()
+{
+    $n = 10;
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+    }
+
+    return $randomString;
 }
 
 ?>
